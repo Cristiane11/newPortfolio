6134b242f3febbf4ca543d217676d00bc4f4da25
Cristiane Abreu <bjwmcd@yahoo.com>
2019-10-06 16:19:54 -0400

    update

--- cris/MyInfo.js
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-import reactDom from 'react-dom';
-
-function MyInfo(){
-
-}
-
-export default MyInfo;
---
+++ cris/src/MyInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function MyInfo(){
+return(
+    <div>
+    <h1>Cristiane Abreu</h1>
+    </div>
+)
+}
+
+export default MyInfo;
