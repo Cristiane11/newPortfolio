37ada3161b2864fab8eb6e2847e950b940e91572
Cristiane Abreu <bjwmcd@yahoo.com>
2019-10-30 06:03:35 -0400

    update

--- cris/src/components/Footer.js
+++ cris/src/components/Footer.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDom from "react-dom";
 
 function Footer(){
+
+    const styles = {
+        color: "#FFFFF", 
+        backgroundColor: "#000000"
+      }
     return(
-        <footer>
+        <footer style={styles}>>
         <h3>Contact Phone</h3>
         <p>704-891-4329</p>
         </footer>
